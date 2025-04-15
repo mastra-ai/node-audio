b8c9a500fb518da6017c96222eb97a87f8f3565f
Ryan Hansen <rphansen91@gmail.com>
2025-04-15 15:33:08 -0700

    audio stream

--- src/lib/microphone.ts
+++ src/lib/microphone.ts
@@ -1,10 +1,11 @@
 import NodeMic, { MicOptions } from "node-mic";
+import { AudioStream } from "node-mic/dist/audio";
 
 export const defaultMicOptions: Partial<MicOptions> = {
   rate: 24100,
 };
 
-export function getMicrophoneStream(options: Partial<MicOptions> = defaultMicOptions): NodeJS.ReadableStream {
+export function getMicrophoneStream(options: Partial<MicOptions> = defaultMicOptions): AudioStream {
   const mic = new NodeMic(options);
   
   mic.start();
