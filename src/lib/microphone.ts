9f885700bedd5e1c8772e7009dd8ddae4ff69139
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-04-15 15:42:23 -0700

    Updates

--- src/lib/microphone.ts
+++ src/lib/microphone.ts
@@ -5,10 +5,10 @@ export const defaultMicOptions: Partial<MicOptions> = {
   rate: 24100,
 };
 
-export function getMicrophoneStream(options: Partial<MicOptions> = defaultMicOptions): AudioStream {
+export function getMicrophoneStream(options: Partial<MicOptions> = defaultMicOptions): { stream: AudioStream, mic: NodeMic } {
   const mic = new NodeMic(options);
-  
+
   mic.start();
-  
-  return mic.getAudioStream();
+
+  return { stream: mic.getAudioStream(), mic };
 }
