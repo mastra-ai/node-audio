4673aa1d82c664c93e5338c80f0ecbd7c4187cdd
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-04-15 16:11:53 -0700

    Updates

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 export { getMicrophone, getMicrophoneStream } from "./lib/microphone";
 export { playAudio } from "./lib/speaker";
-export { recordAudioToFile } from "./lib/recorder";
+export { recordAudioToFile, Recorder } from "./lib/recorder";
 export { createHuddle } from "./lib/huddle";
 export { pathUrlOrStreamToStream } from "./lib/utils";
